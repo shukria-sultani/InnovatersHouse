ee58a22d256dbb671dde3f4631ae9fbb9b0ae850
Shukria <shukriasul5@gmail.com>
2025-07-29 17:03:17 +0430

    Added the Header section and applied styling to it.
    This commit includes:
    - the header html structure
    - styles for header
    - JavaScript functions for creating hamburger menu in small screens.
    - media queries for the responsiveness of the header.

---
+++ src/script.js
@@ -0,0 +1,24 @@
+// responsive header
+
+let menuIcon = document.getElementById("menuIcon");
+let menu = document.querySelector(".header ul");
+menuIcon.addEventListener("click", function() {
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+    } else {
+        menu.style.display = "block";
+    }
+});
+
+// menu text
+
+let menuItems = document.querySelectorAll(".header ul li");
+menuItems.forEach(item => {
+  item.addEventListener("click", function() {
+    let span = this.querySelector(".menuText");
+    if (span) {
+      span.classList.remove("menuText");
+      span.classList.add("clicked-span");
+    }
+  });
+});
